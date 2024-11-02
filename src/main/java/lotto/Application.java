290ffcfa3c26988585c78dfe63e7dfc7bd940d1d
codesooo <zlsoo9ee@gmail.com>
2024-11-02 21:56:26 +0900

    Feat(Application) : 입력 기능 구현 2

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.input.InputReader;
+import lotto.output.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputReader inputReader = new InputReader();
+        OutputView outputView = new OutputView();
+
+        // 로또 구입 금액 입력
+        outputView.printPurchasePrompt();
+        Long purchasePrice = inputReader.purchasePrice();
+
+        // 당첨 번호 입력
+        outputView.printWinningPrompt();
+        List<Integer> winningNumbers = inputReader.winningNumbers();
+
+        // 보너스 번호 입력
+        outputView.printBonusPrompt();
+        int bonusNumber = inputReader.bonusNumber();
     }
 }
